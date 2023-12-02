5a6772689a944625bfdd9fc10ae2e169d28d4e30
Pol472 <128186456+Pol472@users.noreply.github.com>
2023-12-02 14:04:44 -0500

    Fixing Standard Library

--- Library/Sintax/StandardLibrary.cs
+++ Library/Sintax/StandardLibrary.cs
@@ -16,39 +16,6 @@ public static class StandardLibrary
         variables.AddVariable("E", Math.E);
         return variables;
     }
-<<<<<<< Updated upstream
-    
-    private static Dictionary<string, FunctionDeclarationExpression> GetStandardLibraryFunctions()
-    {
-        return new()
-        {
-            ["sin"] = new FunctionDeclarationExpression("sin", new() { "x" }, new FunctionReference((scope) => Math.Sin((double)scope.GetValue("x")))),
-            
-            ["cos"] = new FunctionDeclarationExpression("cos", new() { "x" }, new FunctionReference((scope) => Math.Cos((double)scope.GetValue("x")))),
-
-            ["sqrt"] = new FunctionDeclarationExpression("sqrt", new() { "x" }, new FunctionReference((scope) => Math.Sqrt((double)scope.GetValue("x")))),
-
-            ["log"] = new FunctionDeclarationExpression("log", new() { "b", "p" }, new FunctionReference((scope) => Math.Log((double)scope.GetValue("p"), (double)scope.GetValue("b")))),
-
-            ["log2"] = new FunctionDeclarationExpression("log2", new() { "p" }, new FunctionReference((scope) => Math.Log2((double)scope.GetValue("p")))),
-
-            ["log10"] = new FunctionDeclarationExpression("log10", new() { "p" }, new FunctionReference((scope) => Math.Log10((double)scope.GetValue("p")))),
-
-            ["print"] = new FunctionDeclarationExpression("print", new() { "x" }, new FunctionReference((scope) => scope.GetValue("x").ToString()!)),
-            
-            ["measure"] = new FunctionDeclarationExpression("measure", new() {"p1", "p2"}, new FunctionReference((scope) => Utiles.EuclideanDistance((Point)scope.GetValue("p1"),(Point)scope.GetValue("p2")))  )
-        };
-    }
-}
-public static class Utiles
-{
-    public static double EuclideanDistance(Point p1, Point p2)
-    {
-       double x = Math.Pow((p1.X- p2.X),2);
-       double y = Math.Pow((p1.Y- p2.Y),2);
-       return Math.Sqrt(x+y);
-    }
-=======
 
     private static Dictionary<string, FunctionDeclarationExpression> GetStandardLibraryFunctions()
     {
@@ -73,5 +40,5 @@ public static class Utiles
             ["intercept"] = new FunctionDeclarationExpression("intercept", new() { "c1", "c2" }, new FunctionReference((scope) => Utiles.Interception(scope.GetValue("f1"), scope.GetValue("f1"))))
         };
     }
->>>>>>> Stashed changes
+
 }
